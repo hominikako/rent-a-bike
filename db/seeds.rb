ce7782c41975bb3504a70ac4512d8723870b9341
hominikako <javier.ferrer@student.ncirl.ie>
2016-08-26 18:52:32 +0000

    Add admin user and permissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.new
+user.email = 'javi@admin.com'
+user.name = 'Javi'
+user.last_name = 'Ferrer'
+user.role = 1
+user.password = '123123'
+user.password_confirmation = '123123'
+user.save!
