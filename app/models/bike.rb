d0d4ffd891188155b60772e348919a4574496433
hominikako <javier.ferrer@student.ncirl.ie>
2016-08-26 19:10:55 +0000

    Add status to the bikes

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,13 @@
 class Bike < ActiveRecord::Base
+    
+    # Type of status for a bike
+    def self.what_status_is
+        [:Available, :Reparing]
+    end
+    
+    # Returns a symbol with the status of the bike
+    def status_sym
+        return nil if status.nil?
+        Bike.what_status_is[status]
+    end
 end
