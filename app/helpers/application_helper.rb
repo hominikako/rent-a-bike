48d1905d808961808890d34728964fcaf44729bb
hominikako <javier.ferrer@student.ncirl.ie>
2016-08-27 20:23:38 +0000

    Add style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
          return (user_signed_in? and current_user.role == 1)
     end
     
+    def status_rental_row_class(start_date, end_date)
+        current_time = Time.now.to_date
+        return "danger" if  current_time >= start_date and current_time <= end_date 
+        return "success" if current_time < start_date
+        return "warning" if end_date < current_time
+        return ""
+     end
 end
